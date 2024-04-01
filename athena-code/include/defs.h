2e042238bf0fea3e3e0c4f919ea613903be1a6fe
bytecod3 <emwiti658@gmail.com>
2024-04-01 04:54:38 -0700

    adds wifi scanner task

--- athena-code/include/defs.h
+++ athena-code/include/defs.h
@@ -2,5 +2,8 @@
 #define DEFS_H
 
 #define GPS_BAUDRATE 9600
+#define SD_CS_PIN 5
+
+#define SCAN_DELAY 500
 
 #endif
--- athena-code/src/main.cpp
+++ athena-code/src/main.cpp
@@ -3,6 +3,9 @@
 #include "debug.h"
 #include "WiFiManager.h"
 #include <TinyGPSPlus.h>
+#include "SD.h"
+#include "FS.h"
+#include <freertos/FreeRTOS.h>
 
 // create GPS object
 TinyGPSPlus gps;
@@ -10,7 +13,7 @@ TinyGPSPlus gps;
 const char* WIFI_SSID = "Athena-wardriver";
 const char* WIFI_PASSWORD = "12345678";
 
-void configWIFI();
+void configDynamicWIFI();
 void GPSInit();
 void updateSerial();
 void GPSDisplayInfo();
@@ -19,7 +22,7 @@ void GPSDisplayInfo();
  * Create a AP with SSID and PASSWORD to allow wifi connection to 
  * custom networks 
 */
-void configWIFI() {
+void configDynamicWIFI() {
   debugln("Configuring WIFI");
   WiFiManager wifiManager;
 
@@ -71,10 +74,122 @@ void GPSDisplayInfo() {
 
 }
 
+/**
+ * compare mac address to avoid logging the same network
+*/
+int compareMAC() {
+
+  
+}
+
+void WiFiScanSetup() {
+  // set wifi station mode
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+}
+
+/**
+ * Scan all available wifi networks
+*/
+void scanWIFI(void* parameter) {
+  while(true) {
+    int n = WiFi.scanNetworks();
+    Serial.println("Scan Done");
+
+    if(n == 0) {
+      debugln("No networks found");
+    } else {
+      debug(n);
+      debugln("Networks found");
+
+      for(int i = 0; i < n; i++) {
+        uint8_t* raw_bssid = WiFi.BSSID(i);
+        char bssid[18];
+        sprintf(
+          bssid,
+          "%02X:%02X:%02X:%02X:%02X:%02X",
+          raw_bssid[0],
+          raw_bssid[1],
+          raw_bssid[2],
+          raw_bssid[3],
+          raw_bssid[4],
+          raw_bssid[5]
+        );
+
+        // compare the mac addresses
+
+
+        // print ssid and rssi for each network 
+        debug(i); debug(","); debug(WiFi.SSID(i).c_str());
+        debug(","); debugf("%4d", WiFi.RSSI(i));
+        debug(","); debugf("%2d", WiFi.channel(i));
+        switch (WiFi.encryptionType(i))
+        {
+          case WIFI_AUTH_OPEN:
+                debug("open");
+                break;
+            case WIFI_AUTH_WEP:
+                debug("WEP");
+                break;
+            case WIFI_AUTH_WPA_PSK:
+                debug("WPA");
+                break;
+            case WIFI_AUTH_WPA2_PSK:
+                debug("WPA2");
+                break;
+            case WIFI_AUTH_WPA_WPA2_PSK:
+                Serial.print("WPA+WPA2");
+                break;
+            case WIFI_AUTH_WPA2_ENTERPRISE:
+                debug("WPA2-EAP");
+                break;
+            case WIFI_AUTH_WPA3_PSK:
+                debug("WPA3");
+                break;
+            case WIFI_AUTH_WPA2_WPA3_PSK:
+                debug("WPA2+WPA3");
+                break;
+            case WIFI_AUTH_WAPI_PSK:
+                debug("WAPI");
+                break;
+            default:
+                debug("unknown");
+        }
+
+        debugln();
+
+      }
+
+    }
+  }
+
+  // free memory
+  WiFi.scanDelete();
+
+  // some delay before scanning again
+  delay(SCAN_DELAY);
+}
+
 void setup() {
   Serial.begin(9600);
-  // configWIFI();
+  // configDynamicWIFI();
   GPSInit();
+
+  WiFiScanSetup();
+
+  // print to screen
+  Serial.println("Ready..");
+
+  xTaskCreatePinnedToCore(
+    scanWIFI, 
+    "scanWIFI",
+    10000,
+    NULL, 
+    1,
+    NULL,
+    0
+  );
   
 }
 
@@ -82,9 +197,13 @@ void loop() {
 
   //updateSerial();
 
-  while (Serial2.available() > 0)
-    if (gps.encode(Serial2.read()))
-      GPSDisplayInfo();
+  // while (Serial2.available() > 0) {
+  //   if (gps.encode(Serial2.read())) {
+  //     GPSDisplayInfo();
+  //   }
   
-}
+  // }
 
+
+
+}
