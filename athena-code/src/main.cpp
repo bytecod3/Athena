b85ed9093754cf3e8492853e200e732f437c460f
bytecod3 <emwiti658@gmail.com>
2024-04-02 03:51:40 -0700

    adds logging to SD card

--- athena-code/src/main.cpp
+++ athena-code/src/main.cpp
@@ -5,7 +5,6 @@
 #include <TinyGPSPlus.h>
 #include "SD.h"
 #include "FS.h"
-#include <freertos/FreeRTOS.h>
 
 // create GPS object
 TinyGPSPlus gps;
@@ -18,9 +17,18 @@ typedef struct {
   unsigned char bytes[RAW_BSSID_LENGTH];
 } mac_address;
 
+/* struct to hold latitude and longitude*/
+typedef struct {
+  double latitude;
+  double longitude;
+} Location;
+
+Location location;
+
 /* array to hold scanned MAC addresses */
 mac_address seen_macs[MAX_NETWORKS];
 unsigned int seen_mac_index = 0;
+char data_row[100]; // to hold the row being written to SD Card
 
 void configDynamicWIFI();
 void GPSInit();
@@ -30,11 +38,49 @@ void WiFiScanSetup();
 void seenMAC(mac_address);
 int compareMAC(mac_address, mac_address);
 void saveMAC();
+void SDInit();
+void writeFile(fs::FS &fs, const char * path, const char * message);
+void appendFile(fs::FS &fs, const char * path, const char * message);
+String security_int_to_string(int security_type);
+
+/**
+ * Initialize SD card
+*/
+void SDInit() {
+  SD.begin(SD_CS_PIN); 
+  if(!SD.begin(SD_CS_PIN)) {
+    debugln("Card Mount Failed");
+    return;
+  }
+
+  uint8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE) {
+    debugln("No SD card attached");
+    return;
+  }
+
+  debugln("Initializing SD card...");
+  if (!SD.begin(SD_CS_PIN)) {
+    debugln("ERROR - SD card initialization failed!");
+    return; // init failed
+  }
+
+  // initialize file to log networks to 
+  File file = SD.open("/log.txt");
+  if(!file) {
+    debugln("File does not exist");
+    writeFile(SD, "/log.txt", "SSID, SECURITY, CHANNEL, LAT, LONG, TIME \r\n");
+  } else {
+    debugln("File already exists");
+  }
+
+  file.close();
+
+}
 
 
 /**
- * Create a AP with SSID and PASSWORD to allow wifi connection to 
- * custom networks 
+ * Create a AP with SSID and PASSWORD to allow wifi connection to custom networks 
 */
 void configDynamicWIFI() {
   debugln("Configuring WIFI");
@@ -78,9 +124,12 @@ void updateSerial() {
 void GPSDisplayInfo() {
   debug(F("Location: "));
   if(gps.location.isValid()) {
-    Serial.print(gps.location.lat());
+    debug(gps.location.lat());
+    location.latitude = gps.location.lat();
+
     debug(F(","));
     Serial.print(gps.location.lng(), 6);
+    location.longitude = gps.location.lng();
 
   } else {
     debugln(F("Invalid coordinates"));
@@ -149,6 +198,54 @@ int compareMAC(mac_address mac1, mac_address mac2) {
 
 }
 
+String security_int_to_string(int security_type){
+  //Provide a security type int from WiFi.encryptionType(i) to convert it to a String which Wigle CSV expects.
+  String authtype = "";
+  switch (security_type){
+    case WIFI_AUTH_OPEN:
+      authtype = "[OPEN]";
+      break;
+  
+    case WIFI_AUTH_WEP:
+      authtype = "[WEP]";
+      break;
+  
+    case WIFI_AUTH_WPA_PSK:
+      authtype = "[WPA_PSK]";
+      break;
+  
+    case WIFI_AUTH_WPA2_PSK:
+      authtype = "[WPA2_PSK]";
+      break;
+  
+    case WIFI_AUTH_WPA_WPA2_PSK:
+      authtype = "[WPA_WPA2_PSK]";
+      break;
+  
+    case WIFI_AUTH_WPA2_ENTERPRISE:
+      authtype = "[WPA2]";
+      break;
+
+    //Requires at least v2.0.0 of https://github.com/espressif/arduino-esp32/
+    case WIFI_AUTH_WPA3_PSK:
+      authtype = "[WPA3_PSK]";
+      break;
+
+    case WIFI_AUTH_WPA2_WPA3_PSK:
+      authtype = "[WPA2_WPA3_PSK]";
+      break;
+
+    case WIFI_AUTH_WAPI_PSK:
+      authtype = "[WAPI_PSK]";
+      break;
+        
+    default:
+      authtype = "[UNDEFINED]";
+  }
+
+  return authtype;
+}
+
 
 void WiFiScanSetup() {
   // set wifi station mode
@@ -162,82 +259,45 @@ void WiFiScanSetup() {
 */
 void scanWIFI(void* parameter) {
   while(true) {
-    int n = WiFi.scanNetworks();
-    Serial.println("Scan Done");
-
-    if(n == 0) {
-      debugln(" No networks found");
-    } else {
-      debug(n);
-      debugln(" Networks found");
-
-      for(int i = 0; i < n; i++) {
-        uint8_t* raw_bssid = WiFi.BSSID(i);
-        char bssid[18];
-        sprintf(
-          bssid,
-          "%02X:%02X:%02X:%02X:%02X:%02X",
-          raw_bssid[0],
-          raw_bssid[1],
-          raw_bssid[2],
-          raw_bssid[3],
-          raw_bssid[4],
-          raw_bssid[5]
-        );
-
-        if(seenMAC(raw_bssid)) {
-          debugln("This network seen");
-          continue;
-          
-        }
-
-        saveMAC(raw_bssid);
-
-        // print network details
-        debug(i); debug(","); debugf("%d",raw_bssid); debug(","); debug(WiFi.SSID(i).c_str());
-        debug(","); debugf("%4d", WiFi.RSSI(i));
-        debug(","); debugf("%2d", WiFi.channel(i)); debug(",");
-        
-        switch (WiFi.encryptionType(i)) {
-          case WIFI_AUTH_OPEN:
-                debug("OPEN");
-                break;
-            case WIFI_AUTH_WEP:
-                debug("WEP");
-                break;
-            case WIFI_AUTH_WPA_PSK:
-                debug("WPA");
-                break;
-            case WIFI_AUTH_WPA2_PSK:
-                debug("WPA2");
-                break;
-            case WIFI_AUTH_WPA_WPA2_PSK:
-                Serial.print("WPA+WPA2");
-                break;
-            case WIFI_AUTH_WPA2_ENTERPRISE:
-                debug("WPA2-EAP");
-                break;
-            case WIFI_AUTH_WPA3_PSK:
-                debug("WPA3");
-                break;
-            case WIFI_AUTH_WPA2_WPA3_PSK:
-                debug("WPA2+WPA3");
-                break;
-            case WIFI_AUTH_WAPI_PSK:
-                debug("WAPI");
-                break;
-            default:
-                debug("unknown");
-        }
-
-        debugln();
-
-      }
-
-    }
+    
   }
 
 
+}
+
+
+// Write to the SD card (DON'T MODIFY THIS FUNCTION)
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+// Append data to the SD card (DON'T MODIFY THIS FUNCTION)
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
 }
 
 void setup() {
@@ -245,21 +305,13 @@ void setup() {
   // configDynamicWIFI();
   GPSInit();
 
+  SDInit();
+
   WiFiScanSetup();
 
   // print to screen
   Serial.println("Ready..");
 
-  xTaskCreatePinnedToCore(
-    scanWIFI, 
-    "scanWIFI",
-    10000,
-    NULL, 
-    1,
-    NULL,
-    0
-  );
-  
 }
 
 void loop() {
@@ -272,6 +324,69 @@ void loop() {
   //   }
   
   // }
+
+  int n = WiFi.scanNetworks();
+  debugln("Scan Done");
+
+  if(n == 0) {
+    debugln("No networks found");
+
+  } else {
+    debug(n);
+    debugln(" Networks found");
+
+    // process the scanned networks
+    for(int i = 0; i < n; i++) {
+      uint8_t* raw_bssid = WiFi.BSSID(i);
+      char bssid[18];
+      sprintf(
+        bssid,
+        "%02X:%02X:%02X:%02X:%02X:%02X",
+        raw_bssid[0],
+        raw_bssid[1],
+        raw_bssid[2],
+        raw_bssid[3],
+        raw_bssid[4],
+        raw_bssid[5]
+      );
+
+      if(seenMAC(raw_bssid)) {
+        debugln("This network seen");
+        continue;
+      }
+
+      saveMAC(raw_bssid);
+
+      String ssid = WiFi.SSID(i);
+
+      // log to file 
+      // debug(WiFi.SSID(i)); 
+      // debug(",");
+      // debug(WiFi.RSSI(i));
+      // debug(","); 
+      // debug(security_int_to_string(WiFi.encryptionType(i)));
+      // debug(","); 
+      // debug(WiFi.channel(i));
+      // debugln();
+
+      sprintf(data_row,
+              "%s, %s, %d, %d \n\r",
+              ssid.c_str(),
+              security_int_to_string(WiFi.encryptionType(i)),
+              WiFi.channel(i),
+              WiFi.RSSI(i)           
+      );
+
+      debugln(data_row);
+
+      // log to SD card
+      appendFile(SD, "/log.txt", data_row);
+
+    }
+
+  }
+
+  WiFi.scanDelete();
 
 
 }
